d9b99c804b66ac91cbc5e1f019f428277f51cc82
Prince Forson <prince.forson@ssense.com>
2023-09-28 03:39:13 -0400

    Fixed some bugs in hour picked total

--- hourly_pick_totals.py
+++ hourly_pick_totals.py
@@ -1,92 +1,28 @@
-# hourly_pick_totals.py
-
-import openpyxl
 import pandas as pd
+import openpyxl
 from openpyxl.utils.dataframe import dataframe_to_rows
 
+# Define the time range
+START_TIME = pd.to_datetime('8:00 PM', format='%I:%M %p')
+END_TIME = pd.to_datetime('11:59 PM', format='%I:%M %p')
 
-START_TIME_PUTWALL = pd.to_datetime('8:00 PM', format='%I:%M %p')
-END_TIME_PUTWALL = pd.to_datetime('11:59 PM', format='%I:%M %p')
-
-def calculate_regular_pick_totals(df):
-    # Initialize a dictionary to store regular pick totals per hour
-    regular_pick_totals = {hour: 0 for hour in range(20, 24)}
-
-    for index, row in df.iterrows():
-        action = row['Action']
-        datetime = row['DateTime']
-
-        if action == 'REGULAR PICK':
-            hour = datetime.hour
-
-            if hour >= 20 and hour < 24:
-                regular_pick_totals[hour] += row['Quantity']
-
-    return regular_pick_totals
-
-def calculate_single_pick_totals(df):
-    # Initialize a dictionary to store single pick totals per hour
-    single_pick_totals = {hour: 0 for hour in range(20, 24)}
-
-    for index, row in df.iterrows():
-        action = row['Action']
-        packslip = row['Packslip']
-        datetime = row['DateTime']
-
-        if action == 'REPLNISH' and len(str(packslip)) >= 7 and str(packslip)[6] == 'S' and datetime >= START_TIME_PUTWALL and datetime <= END_TIME_PUTWALL:
-            hour = datetime.hour
-
-            if hour >= 20 and hour < 24:
-                single_pick_totals[hour] += row['Quantity']
-
-    return single_pick_totals
-
-def calculate_replenishment_pick_totals(df):
-    # Initialize a dictionary to store replenishment pick totals per hour
-    replenishment_pick_totals = {hour: 0 for hour in range(20, 24)}
-
-    for index, row in df.iterrows():
-        action = row['Action']
-        packslip = row['Packslip']
-        datetime = row['DateTime']
-
-        if action == 'REPLNISH' and len(str(packslip)) >= 7 and str(packslip)[6] == 'P' and datetime >= START_TIME_PUTWALL and datetime <= END_TIME_PUTWALL:
-            hour = datetime.hour
-
-            if hour >= 20 and hour < 24:
-                replenishment_pick_totals[hour] += row['Quantity']
-
-    return replenishment_pick_totals
-
-def calculate_putwall_pick_totals(df):
-
-    # Initialize a dictionary to store putwall pick totals per hour
-    putwall_pick_totals = {hour: 0 for hour in range(20, 24)}
-
-    for index, row in df.iterrows():
-        action = row['Action']
-        bin_label = row['BinLabel']
-
-        if action == 'PUTWALL PICKING' and bin_label.startswith('MW'):
-            hour = row['DateTime'].hour
-
-            if hour >= 20 and hour < 24:
-                putwall_pick_totals[hour] += row['Quantity']
-
-    return putwall_pick_totals
+def calculate_pick_totals(df, action_filter):
+    # Filter the DataFrame by action_filter and time range
+    filtered_df = df[(df['Action'].isin(action_filter)) & (df['DateTime'].dt.hour >= 20) & (df['DateTime'].dt.hour < 24)]
+    
+    # Group by hour and sum quantities
+    pick_totals = filtered_df.groupby(filtered_df['DateTime'].dt.hour)['Quantity'].sum().to_dict()
+    
+    return pick_totals
 
 def perform_hourly_pick_totals_analysis(df, book):
-    # Calculate Regular Pick totals per hour
-    regular_pick_totals = calculate_regular_pick_totals(df)
-
-    # Calculate Single Pick totals per hour
-    single_pick_totals = calculate_single_pick_totals(df)
-
-    # Calculate Replenishment Pick totals per hour
-    replenishment_pick_totals = calculate_replenishment_pick_totals(df)
-
-    # Calculate Putwall Pick totals per hour
-    putwall_pick_totals = calculate_putwall_pick_totals(df)
+    # Define the pick types and corresponding action filters
+    pick_types = {
+        'Regular Pick': ['REGULAR PICK'],
+        'Single Pick': ['SINGLE PICK'],
+        'Replenishment Pick': ['REPLENISHMENT PICK'],
+        'Putwall Pick': ['PUTWALL PICKING']
+    }
 
     # Create the "Total Units picked by hour" sheet if it doesn't exist
     if 'Total Units picked by hour' not in book.sheetnames:
@@ -95,28 +31,27 @@ def perform_hourly_pick_totals_analysis(df, book):
         hourly_pick_totals_sheet = book['Total Units picked by hour']
 
     # Write the header row
-    header_row = ['Hour', 'Regular Pick', 'Single Pick', 'Replenishment Pick', 'Putwall Pick']
+    header_row = ['Hour'] + list(pick_types.keys())
     hourly_pick_totals_sheet.append(header_row)
 
-    # Calculate the total quantity for each hour and write to the sheet
+    # Calculate and write the total quantity for each hour and pick type
     for hour in range(20, 24):
-        regular_pick_quantity = regular_pick_totals.get(hour, 0)
-        single_pick_quantity = single_pick_totals.get(hour, 0)
-        replenishment_pick_quantity = replenishment_pick_totals.get(hour, 0)
-        putwall_pick_quantity = putwall_pick_totals.get(hour, 0)
+        hour_data = [hour]
 
-        # Create a row with quantities for each pick type
-        hour_data = [hour, regular_pick_quantity, single_pick_quantity, replenishment_pick_quantity, putwall_pick_quantity]
+        for pick_type, action_filter in pick_types.items():
+            pick_totals = calculate_pick_totals(df, action_filter)
+            quantity = pick_totals.get(hour, 0)
+            hour_data.append(quantity)
+
         hourly_pick_totals_sheet.append(hour_data)
 
-    # Calculate the total quantities for each pick type
-    total_regular_pick = sum(regular_pick_totals.values())
-    total_single_pick = sum(single_pick_totals.values())
-    total_replenishment_pick = sum(replenishment_pick_totals.values())
-    total_putwall_pick = sum(putwall_pick_totals.values())
+    # Calculate and write the total quantities for each pick type
+    total_row = ['Total']
+    for pick_type, action_filter in pick_types.items():
+        pick_totals = calculate_pick_totals(df, action_filter)
+        total_quantity = sum(pick_totals.values())
+        total_row.append(total_quantity)
 
-    # Add the totals row to the sheet
-    total_row = ['Total', total_regular_pick, total_single_pick, total_replenishment_pick, total_putwall_pick]
     hourly_pick_totals_sheet.append(total_row)
 
     # Save the Excel file
@@ -124,3 +59,7 @@ def perform_hourly_pick_totals_analysis(df, book):
     book.save(output_excel_file)
 
     print("Hourly pick totals analysis completed and saved.")
+
+# Usage:
+# Call perform_hourly_pick_totals_analysis with your DataFrame and Excel book as arguments
+# Example: perform_hourly_pick_totals_analysis(df, openpyxl.Workbook())
